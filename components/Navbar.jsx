88cf3a0233beb202b2fe2cf73c86a75c399a8aea
Mohammed Al-Musawi <mohue03@gmail.com>
2023-08-16 15:34:35 -0400

    Initial Commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,98 @@
+import Link from 'next/link'
+import React, {useState} from 'react'
+import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai'
+import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    return (
+    <div className='fixed w-full h-[4rem] shadow-xl z-[100]'>
+        <div className='flex md:justify-center items-center w-full h-full px-2 2xl:px-16'>
+            <div>
+                <ul className='gradient hidden md:flex'>
+                    <Link href='/'>
+                        <li className='navbar_text'>About</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='navbar_text'>Projects</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='navbar_text'>Heaven Sent</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='navbar_text'>Media</li>
+                    </Link>
+                    <Link href='/'>
+                        <li className='navbar_text'>Contact</li>
+                    </Link>
+                </ul>
+                <div onClick={handleNav} className='ml-4 cursor-pointer md:hidden'>
+                    <AiOutlineMenu size={25}/>
+                </div>
+            </div>
+        </div> 
+        <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/50' : ''}>
+            <div className={
+                nav ?
+                'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+            }>
+                <div>
+                    <div className='flex w-full justify-between'>
+                        <div></div>
+                        <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-[1.15] ease-in duration-300'>
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'>
+                        <p className='w-[85%] md-[90%] py-4'>Each consistent effort lays another brick towards the monument of greatness.</p> 
+                    </div>
+                </div>
+                <div>
+                    <ul className='py-4 flex-col'>
+                        <Link href='/'>
+                            <li className='py-4 text-sm uppercase hover:text-white ease-in duration-200'>About</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm uppercase hover:text-white ease-in duration-200'>Projects</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm uppercase hover:text-white ease-in duration-200'>Heaven Sent</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm uppercase hover:text-white ease-in duration-200'>Media</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm uppercase hover:text-white ease-in duration-200'>Contact</li>
+                        </Link>
+                    </ul>
+                    <div className='pt-40'>
+                        <p className='uppercasae tracking-widest text-[#5651e5]'>Connect with me!</p>
+                        <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-[1.15] ease-in duration-300'>
+                                <FaGithub />
+                            </div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-[1.15] ease-in duration-300'>
+                                <FaLinkedinIn />
+                            </div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-[1.15] ease-in duration-300'>
+                                <AiOutlineMail />
+                            </div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-[1.15] ease-in duration-300'>
+                                <BsFillPersonLinesFill />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        text: 'text 5s ease infinite',
       },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center',
+          },
+        },
+      },
+      fontFamily: {
+        'lostar': ['Lostar', 'sans-serif'],
+        'magicretro': ['Magic Retro', 'sans-serif']
+      }
     },
   },
   plugins: [],
