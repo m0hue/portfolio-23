6b787cf963ed4d95e5621b39a61b3c4c2cce9c4e
Mohammed Al-Musawi <mohue03@gmail.com>
2023-09-11 22:58:54 -0400

    Updated indexes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import { Inter } from 'next/font/google'
-import Head from 'next/head'
-import Navbar from '@/components/Navbar'
-import Main from '@/components/Main'
-import About from '@/components/About'
-import Skills from '@/components/Skills'
-import Projects from '@/components/Projects'
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import Main from "@/components/Main";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,13 +15,14 @@ export default function Home() {
       <Head>
         <title>M. Al-Musawi | Portfolio</title>
         <meta name="description" content="Mohammed Al-Musawi's Portfolio" />
-        <link rel="icon" href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <Main />
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
-  )
+  );
 }
