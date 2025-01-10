04473700a5b496e3f3899687b3375e4eda85e3f0
Mohammed Al-Musawi <mohue03@gmail.com>
2025-01-09 22:15:10 -0500

    Changed 'Hello' to 'Hi there'

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-300">
-            Hello!
+            Hi there!
           </p>
           <h1 className="py-4">
             I&apos;m <span className="gradient-1-text">Mohue</span>!
